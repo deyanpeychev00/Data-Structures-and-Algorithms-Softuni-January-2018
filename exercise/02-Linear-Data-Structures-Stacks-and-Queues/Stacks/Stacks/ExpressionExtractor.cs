14178db3f8a834f1efcc87504d95b52aafdcf97a
Deyan Peychev <deyan.p.peychev@gmail.com>
2018-01-18 16:48:04 +0200

    Added Stack exercise task

---
+++ exercise/02-Linear-Data-Structures-Stacks-and-Queues/Stacks/Stacks/ExpressionExtractor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Stacks
+{
+    class ExpressionExtractor
+    {
+        static void Main()
+        {
+            // Sample input:  1 + (2 - (2 + 3) * 4 / (3 + 1)) * 5
+
+            string expression = Console.ReadLine();
+            var bracketStack = new Stack<int>();
+
+            for (int i = 0; i < expression.Length; i++)
+            {
+                if(expression[i] == '(')
+                {
+                    bracketStack.Push(i);
+                }
+                else if(expression[i] == ')')
+                {
+                    var bracketFrom = bracketStack.Pop();
+
+                    Console.WriteLine(expression.Substring(bracketFrom, (i-bracketFrom)+1));
+                }
+            }
+        }
+    }
+}
