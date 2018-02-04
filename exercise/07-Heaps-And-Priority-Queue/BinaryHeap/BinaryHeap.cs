1a2adc71a53fac318e01ce7b27df92c70e9213df
Deyan Peychev <deyan.p.peychev@gmail.com>
2018-02-04 19:49:33 +0200

    Implemented Heap and Priority Queue

--- exercise/07-Heaps-And-Priority-Queue/BinaryHeap/BinaryHeap.cs
+++ exercise/07-Heaps-And-Priority-Queue/BinaryHeap/BinaryHeap.cs
@@ -7,29 +7,92 @@ public class BinaryHeap<T> where T : IComparable<T>
 
     public BinaryHeap()
     {
-        // TODO
+        this.heap = new List<T>();
     }
 
     public int Count
     {
         get
         {
-            throw new NotImplementedException();
+            return this.heap.Count;
         }
     }
 
     public void Insert(T item)
     {
-        throw new NotImplementedException();
+        heap.Add(item);
+        this.HeapifyUp(this.heap.Count - 1);
+    }
+
+    private void HeapifyUp(int childIndex)
+    {
+        int parentIndex = (childIndex - 1) / 2;
+        int compare = this.heap[parentIndex].CompareTo(this.heap[childIndex]);
+
+        if (compare < 0)
+        {
+            this.Swap(parentIndex, childIndex);
+            this.HeapifyUp(parentIndex);
+        }
+
+    }
+    
+    private void Swap(int parentIndex, int childIndex)
+    {
+        var temp = this.heap[parentIndex];
+        this.heap[parentIndex] = this.heap[childIndex];
+        this.heap[childIndex] = temp;
     }
 
     public T Peek()
     {
-        throw new NotImplementedException();
+        if (this.heap.Count == 0)
+        {
+            throw new InvalidOperationException();
+        }
+
+        return this.heap[0];
     }
 
     public T Pull()
     {
-        throw new NotImplementedException();
+        if (this.heap.Count == 0)
+        {
+            throw new InvalidOperationException();
+        }
+
+        var maxHeapElement = this.heap[0];
+        this.Swap(0, this.heap.Count - 1);
+        this.heap.RemoveAt(this.heap.Count - 1);
+        this.HeapifyDown(0);
+
+        return maxHeapElement;
+    }
+
+    private void HeapifyDown(int rootIndex)
+    {
+
+        while(rootIndex < this.Count / 2)
+        {
+            int childIndex = (rootIndex * 2) + 1;
+            if (childIndex + 1 < this.Count && ChildIsGreater(childIndex, childIndex + 1))
+            {
+                childIndex += 1;
+            }
+
+            int compare = this.heap[rootIndex].CompareTo(this.heap[childIndex]);
+
+            if (compare < 0)
+            {
+                this.Swap(rootIndex, childIndex);
+            }
+            rootIndex = childIndex;
+        }
+
+    }
+
+    private bool ChildIsGreater(int leftChildIndex, int rightChildIndex)
+    {
+        return this.heap[leftChildIndex].CompareTo(this.heap[rightChildIndex]) < 0;
     }
 }
--- exercise/07-Heaps-And-Priority-Queue/BinaryHeap/Heap.cs
+++ exercise/07-Heaps-And-Priority-Queue/BinaryHeap/Heap.cs
@@ -4,6 +4,59 @@ public static class Heap<T> where T : IComparable<T>
 {
     public static void Sort(T[] arr)
     {
-        throw new NotImplementedException();
+        MakeHeap(arr);
+        HeapSort(arr);
+    }
+
+    private static void HeapSort(T[] arr)
+    {
+        for (int i = arr.Length - 1; i > 0; i--)
+        {
+            Swap(arr, 0, i);
+            HeapifyDown(arr, 0, i);
+
+        }
+    }
+
+    private static void MakeHeap(T[] arr)
+    {
+        for( int i = arr.Length / 2; i >= 0; i--)
+        {
+             HeapifyDown(arr, i, arr.Length);
+        }
+    }
+
+    private static void HeapifyDown(T[] array, int rootIndex, int arrLength)
+    {
+
+        while (rootIndex < arrLength / 2)
+        {
+            int childIndex = (rootIndex * 2) + 1;
+            if (childIndex + 1 < arrLength && ChildIsGreater(array, childIndex, childIndex + 1))
+            {
+                childIndex += 1;
+            }
+
+            int compare = array[rootIndex].CompareTo(array[childIndex]);
+
+            if (compare < 0)
+            {
+                Swap(array, rootIndex, childIndex);
+            }
+            rootIndex = childIndex;
+        }
+
+    }
+
+    private static void Swap(T[] array, int parentIndex, int childIndex)
+    {
+        var temp = array[parentIndex];
+        array[parentIndex] = array[childIndex];
+        array[childIndex] = temp;
+    }
+
+    private static bool ChildIsGreater(T[] array, int leftChildIndex, int rightChildIndex)
+    {
+        return array[leftChildIndex].CompareTo(array[rightChildIndex]) < 0;
     }
 }
