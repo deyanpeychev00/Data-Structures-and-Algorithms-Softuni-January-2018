3bf361deb62e1280f085a022a3396cf75921b009
Deyan Peychev <deyan.p.peychev@gmail.com>
2018-02-25 14:05:26 +0200

    Implemented IntervalTree SearchAll & SearchAny functionality.

--- exercise/13-Quad-Trees-K-d-Trees-Interval-Trees-Lab/Interval-Tree/IntervalTree/IntervalTree.cs
+++ exercise/13-Quad-Trees-K-d-Trees-Interval-Trees-Lab/Interval-Tree/IntervalTree/IntervalTree.cs
@@ -5,15 +5,15 @@ public class IntervalTree
 {
     private class Node
     {
-        internal Interval interval;
-        internal double max;
-        internal Node right;
-        internal Node left;
+        internal Interval Interval;
+        internal double MaxEndpoint;
+        internal Node RightInterval;
+        internal Node LeftInterval;
 
         public Node(Interval interval)
         {
-            this.interval = interval;
-            this.max = interval.Hi;
+            this.Interval = interval;
+            this.MaxEndpoint = interval.Hi;
         }
     }
 
@@ -31,12 +31,48 @@ public class IntervalTree
 
     public Interval SearchAny(double lo, double hi)
     {
-        throw new NotImplementedException();
+        Node currentInterval = this.root;
+        while(currentInterval != null && !currentInterval.Interval.Intersects(lo, hi))
+        {
+            if(currentInterval.LeftInterval != null && currentInterval.LeftInterval.MaxEndpoint > lo)
+            {
+                currentInterval = currentInterval.LeftInterval;
+            }
+            else
+            {
+                currentInterval = currentInterval.RightInterval;
+            }
+        }
+
+        return currentInterval?.Interval;
     }
 
     public IEnumerable<Interval> SearchAll(double lo, double hi)
     {
-        throw new NotImplementedException();
+        List<Interval> resultList = new List<Interval>();
+        this.SearchAll(this.root, resultList, new Interval(lo, hi));
+        return resultList;
+    }
+
+    private void SearchAll(Node node, List<Interval> intervalsList, Interval interval)
+    {
+        if(node == null)
+        {
+            return;
+        }
+
+        if (node.LeftInterval != null && node.LeftInterval.MaxEndpoint > interval.Lo)
+        {
+            this.SearchAll(node.LeftInterval, intervalsList, interval);
+        }
+        if (node.Interval.Intersects(interval.Lo, interval.Hi))
+        {
+            intervalsList.Add(node.Interval);
+        }
+        if(node.RightInterval != null && node.RightInterval.Interval.Lo < interval.Hi)
+        {
+            this.SearchAll(node.RightInterval, intervalsList, interval);
+        }
     }
 
     private void EachInOrder(Node node, Action<Interval> action)
@@ -46,9 +82,9 @@ public class IntervalTree
             return;
         }
 
-        EachInOrder(node.left, action);
-        action(node.interval);
-        EachInOrder(node.right, action);
+        EachInOrder(node.LeftInterval, action);
+        action(node.Interval);
+        EachInOrder(node.RightInterval, action);
     }
 
     private Node Insert(Node node, double lo, double hi)
@@ -58,16 +94,36 @@ public class IntervalTree
             return new Node(new Interval(lo, hi));
         }
 
-        int cmp = lo.CompareTo(node.interval.Lo);
+        int cmp = lo.CompareTo(node.Interval.Lo);
         if (cmp < 0)
         {
-            node.left = Insert(node.left, lo, hi);
+            node.LeftInterval = Insert(node.LeftInterval, lo, hi);
         }
         else if (cmp > 0)
         {
-            node.right = Insert(node.right, lo, hi);
+            node.RightInterval = Insert(node.RightInterval, lo, hi);
         }
-        
+        UpdateMaxEndpoint(node);
         return node;
+    }
+
+    private void UpdateMaxEndpoint(Node node)
+    {
+        var maxChild = GetMax(node.LeftInterval, node.RightInterval);
+        node.MaxEndpoint = GetMax(node, maxChild).MaxEndpoint;
+    }
+
+    private Node GetMax(Node left, Node right)
+    {
+        if(left == null)
+        {
+            return right;
+        }
+        if(right == null)
+        {
+            return left;
+        }
+
+        return left.MaxEndpoint > right.MaxEndpoint ? left : right;
     }
 }
