9ac4f15071d1ff80e5a67e65607746645d0794c7
Deyan Peychev <deyan.p.peychev@gmail.com>
2018-03-05 19:30:34 +0200

    Started solving old exams | First sample exam (09.Sep.2017) | Task #1 - Invaders | Stuck at 74/100

---
+++ exam/Data-Structures-Retake-Exam-09-Sep-2017-C#/Invaders/Invaders/Computer.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Computer : IComputer
+{
+
+    private List<Invader> records;
+    private int energy;
+
+    public Computer(int energy)
+    {
+        this.records = new List<Invader>();
+        if(energy < 0)
+        {
+            throw new ArgumentException();
+        }
+        this.energy = energy;
+
+    }
+
+    public int Energy
+    {
+        get
+        {
+            return this.energy < 0 ? 0 : this.energy;
+        }
+    }
+
+    public void Skip(int turns)
+    {
+        for (int i = 0; i < this.records.Count; i++)
+        {
+            this.records[i].Distance -= turns;
+            if (this.records[i].Distance <= 0)
+            {
+                this.energy -= this.records[i].Damage;
+                this.records.RemoveAt(i);
+                i--;
+            }
+        }
+    }
+
+    public void AddInvader(Invader invader)
+    {
+        this.records.Add(invader);
+    }
+
+    public void DestroyHighestPriorityTargets(int count)
+    {
+
+        if (this.records.Count < count)
+        {
+            this.records.Clear();
+            return;
+        }
+        this.records.Sort();
+        this.records.RemoveRange(0, count);
+    }
+
+    public void DestroyTargetsInRadius(int radius)
+    {
+        this.records.RemoveAll(x => x.Distance <= radius);
+    }
+
+    public IEnumerable<Invader> Invaders()
+    {
+        foreach (var invader in this.records)
+        {
+            yield return invader;
+        }
+    }
+}
---
+++ exam/Data-Structures-Retake-Exam-09-Sep-2017-C#/Invaders/Invaders/Invader.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Invader : IInvader
+{
+    public Invader(int damage, int distance)
+    {
+        this.Distance = distance;
+        this.Damage = damage;
+    }
+    
+    public int Damage { get; set; }
+    public int Distance { get; set; }
+
+    public int CompareTo(IInvader other)
+    {
+        return this.Distance.CompareTo(other.Distance) != 0 ? this.Distance.CompareTo(other.Distance) : other.Damage.CompareTo(this.Damage);
+    }
+}
